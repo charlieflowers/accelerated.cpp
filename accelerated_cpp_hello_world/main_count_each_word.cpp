c31ea09523d5d5e02436eab73683761508fa67f3
charlieflowers <cflowers@gmail.com>
2013-06-18 22:59:30 -0400

    shit

--- accelerated_cpp_hello_world/main_count_each_word.cpp
+++ accelerated_cpp_hello_world/main_count_each_word.cpp
@@ -1,55 +1,55 @@
-/*
- * main_count_each_word.cpp
- *
- *  Created on: Jun 18, 2013
- *      Author: cflowers
- */
-
-#include <vector>;
-#include <iostream>;
-#include <string>;
-#include <algorithm>;
-
-#include "read_words.h";
-
-using std::cin;			using std::cout;
-using std::vector;		using std::endl;
-using std::string;		using std::sort;
-
-int main() {
-
-	vector<string> words;
-	read_words(cin, words);
-
-	sort(words.begin(), words.end());
-
-	int index = 0;
-	string word_to_match = "QQQQQ"; // sentinel because he hasn't covered some other concepts.
-
-	int word_count;
-
-	while(index < words.size()) {
-		string this_word = words[index];
-
-		if(this_word == word_to_match) {
-			word_count++;
-		} else {
-			// I wrote this kind of crazy for the hell of it, something was telling
-			//  me this should work. Not readable, I wouldn't do it in real code.
-			if(word_to_match != "QQQQQ") cout << word_count << " times!" << endl;
-
-			cout << "The word " << this_word << " appears ";
-			word_to_match = this_word;
-			word_count = 1;
-		}
-
-		index++;
-	}
-
-	if(word_to_match != "QQQQQ") cout << word_count << " times!" << endl;
-
-	return 0;
-}
-
-
-
+///*
+// * main_count_each_word.cpp
+// *
+// *  Created on: Jun 18, 2013
+// *      Author: cflowers
+// */
+//
+//#include <vector>;
+//#include <iostream>;
+//#include <string>;
+//#include <algorithm>;
+//
+//#include "read_words.h";
+//
+//using std::cin;			using std::cout;
+//using std::vector;		using std::endl;
+//using std::string;		using std::sort;
+//
+//int main() {
+//
+//	vector<string> words;
+//	read_words(cin, words);
+//
+//	sort(words.begin(), words.end());
+//
+//	int index = 0;
+//	string word_to_match = "QQQQQ"; // sentinel because he hasn't covered some other concepts.
+//
+//	int word_count;
+//
+//	while(index < words.size()) {
+//		string this_word = words[index];
+//
+//		if(this_word == word_to_match) {
+//			word_count++;
+//		} else {
+//			// I wrote this kind of crazy for the hell of it, something was telling
+//			//  me this should work. Not readable, I wouldn't do it in real code.
+//			if(word_to_match != "QQQQQ") cout << word_count << " times!" << endl;
+//
+//			cout << "The word " << this_word << " appears ";
+//			word_to_match = this_word;
+//			word_count = 1;
+//		}
+//
+//		index++;
+//	}
+//
+//	if(word_to_match != "QQQQQ") cout << word_count << " times!" << endl;
+//
+//	return 0;
+//}
+//
+//
+//
--- accelerated_cpp_hello_world/main_student_grades.cpp
+++ accelerated_cpp_hello_world/main_student_grades.cpp
@@ -1,62 +1,62 @@
-///*
-// * main.cpp
-// *
-// *  Created on: Jun 18, 2013
-// *      Author: cflowers
-// */
-//
-//#include <iostream>;
-//#include <iomanip>;
-//#include <vector>;
-//#include <algorithm>;
-//#include <stdexcept>;
-//
-//#include "median.h";
-//#include "Student_info.h";
-//#include "grade.h";
-//
-//using std::cout;				using std::endl;
-//using std::vector;				using std::cin;
-//using std::string;				using std::max;
-//using std::streamsize;			using std::setprecision;
-//using std::domain_error;
-//
-//int main() {
-//
-//	// For the first time in the book, we're reading from a file and not human input.
-//	vector<Student_info> students;
-//
-//	typedef string::size_type string_sz;
-//	string_sz longest = 0;
-//
-//	Student_info s;
-//	while(read(cin, s)) {
-//		students.push_back(s);
-//		longest = max(longest, s.name.size());
-//	}
-//
-//	// He wants the students in alphabetical order
-//	sort(students.begin(), students.end(), compare);
-//
-//	typedef vector<Student_info>::size_type vec_sz;
-//	vec_sz count = students.size();
-//
-//	for(vec_sz c = 0; c != count; c++) {
-//		cout << students[c].name << string(longest + 1 - students[c].name.size(), ' ');
-//
-//		try {
-//			double final_grade = grade(students[c]);
-//
-//			streamsize prec = cout.precision();
-//			cout << setprecision(3) << final_grade << setprecision(prec);
-//		} catch(domain_error& e) {
-//			cout << "An error occurred: " << e.what();
-//		}
-//
-//		cout << endl;
-//	}
-//
-//	return 0;
-//}
-//
-//
+/*
+ * main.cpp
+ *
+ *  Created on: Jun 18, 2013
+ *      Author: cflowers
+ */
+
+#include <iostream>;
+#include <iomanip>;
+#include <vector>;
+#include <algorithm>;
+#include <stdexcept>;
+
+#include "median.h";
+#include "Student_info.h";
+#include "grade.h";
+
+using std::cout;				using std::endl;
+using std::vector;				using std::cin;
+using std::string;				using std::max;
+using std::streamsize;			using std::setprecision;
+using std::domain_error;
+
+int main() {
+
+	// For the first time in the book, we're reading from a file and not human input.
+	vector<Student_info> students;
+
+	typedef string::size_type string_sz;
+	string_sz longest = 0;
+
+	Student_info s;
+	while(read(cin, s)) {
+		students.push_back(s);
+		longest = max(longest, s.name.size());
+	}
+
+	// He wants the students in alphabetical order
+	sort(students.begin(), students.end(), compare);
+
+	typedef vector<Student_info>::size_type vec_sz;
+	vec_sz count = students.size();
+
+	for(vec_sz c = 0; c != count; c++) {
+		cout << students[c].name << string(longest + 1 - students[c].name.size(), ' ');
+
+		try {
+			double final_grade = grade(students[c]);
+
+			streamsize prec = cout.precision();
+			cout << setprecision(3) << final_grade << setprecision(prec);
+		} catch(domain_error& e) {
+			cout << "An error occurred: " << e.what();
+		}
+
+		cout << endl;
+	}
+
+	return 0;
+}
+
+
